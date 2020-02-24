bf82057fff35f174a42c7064586eb61da0713d1e
Rameez Khan <rameez.ka.khan@bmwgroup.com>
2020-02-24 09:13:08 +0200

    feat: app returns request

---
+++ app.py
@@ -0,0 +1,17 @@
+import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.requests import Request
+from starlette.routing import Route
+
+
+async def root(request: Request):
+    data = await request.json()
+    return JSONResponse({"response": data})
+
+
+app = Starlette(debug=True, routes=[Route("/", root, methods=["POST"])])
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=5000)
