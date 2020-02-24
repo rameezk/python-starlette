c91d263a3faf76f03d2176abec21da02db3c9ed1
Rameez Khan <rameez.ka.khan@bmwgroup.com>
2020-02-24 11:57:30 +0200

    feat: bind to port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker --log-level warning app:app
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker --log-level info app:app -b "0.0.0.0:5000"
