963cc675df38ee321b10e70cfe46f484cfdef120
Rameez Khan <rameez.ka.khan@bmwgroup.com>
2020-02-25 07:29:36 +0200

    feat: connect to sqlite

--- src/app.py
+++ src/app.py
@@ -1,20 +1,31 @@
 import uvicorn
 import os
 from starlette.applications import Starlette
+import databases
 
 from api import get_routes
 from api.handler.exception import exception_handlers
 
-environment = os.environ.get("ENVIRONMENT")
+ENVIRONMENT = os.environ.get("ENVIRONMENT")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 
 debug = False
 reload = False
-if environment == "DEV":
+if ENVIRONMENT == "DEV":
     debug = True
     reload = True
 
+database = databases.Database(DATABASE_URL)
 
-app = Starlette(routes=get_routes(), debug=debug, exception_handlers=exception_handlers)
+
+app = Starlette(
+    routes=get_routes(),
+    on_startup=[database.connect],
+    on_shutdown=[database.disconnect],
+    debug=debug,
+    exception_handlers=exception_handlers,
+)
 
 
 if __name__ == "__main__":
